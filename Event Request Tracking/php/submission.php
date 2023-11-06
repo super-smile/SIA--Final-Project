998e53474e0aa7b64fe7cae93cd6a722883474db
Kevin Fabregas <143676079+IrelleFabregas@users.noreply.github.com>
2023-11-06 21:15:04 +0800

    ito ay dun sa alert ng confirm submission

--- Event Request Tracking/php/submission.php
+++ Event Request Tracking/php/submission.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type= "text/css" href="stylesubmission.css">
+    <link rel="stylesheet" type="text/css" href="stylesubmission.css">
+
     <title>Submission Confirmation</title>
 </head>
+
 <body>
     <div class="container">
-         <form id="submission-confirm">
+        <form id="submission-confirm">
             <p class="title">Submission Confirm</p>
-            <p class="subtitle">Are you certain you want to submit this? The information you've entered <strong> cannot be altered or reversed </strong> once it's been submitted.</p>
+            <p class="subtitle">Are you certain you want to submit this? The information you've entered <b> cannot be altered or reversed</b>
+                once it's been submitted.
+            </p>
 
-        <button class="button ConfirmSubmission">Confirm Submission</button>
-        <button class="button GoBack">Go Back</button>
+            <button class="ConfirmSubmission button">Confirm Submission</button>
+
+        </form>
+
+        <a href="letter.php" class="GoBack button">
+            Go Back
+        </a>
+
     </div>
 </body>
+
 </html>
