89910414fd8a5251b72e8544c8cf11e6357798a9
Kevin Fabregas <143676079+IrelleFabregas@users.noreply.github.com>
2023-11-15 20:20:08 +0800

    ETO ANG DATABASE NG UPLOAD LETTER #ILONG

--- Event Request Tracking/php/letter.php
+++ Event Request Tracking/php/letter.php
@@ -16,18 +16,20 @@
 <body onload="uploadPDF()">
     <div class="container">
 
-        <form id="upload-letter">
+        <form id="upload-letter" method="POST" action="upload.php" enctype="multipart/form-data">
+
+
             <p class="title">Upload Letter</p>
             <p class="subtitle">Please complete the event information.</p>
 
             <div class="input-event-name">
-                <label for="event-name">Event Name:</label>
-                <input type="text" name="event-name" id="event-name" required />
+                <label for="eventName">Event Name:</label>
+                <input type="text" name="eventName" id="eventName" required />
             </div>
 
             <div class="input-event-date">
-                <label for="event-date">Event Date:</label>
-                <input type="date" name="event-date" id="event-date" required />
+                <label for="eventDate">Event Date:</label>
+                <input type="date" name="eventDate" id="eventDate" required />
             </div>
 
             <div class="subcontainer">
@@ -50,9 +52,8 @@
             <p class="text1">Selsect a file from the computer or drag and drop here.</p>
             <p class="text2">For the best results, please submit a PDF file.</p>
 
-            <input type="file" id="pdfFile" name="pdfFile" accept=".pdf, .jpg, .png" style="display: none; visibility: visible;">
+            <input type="file" id="pdfFile" name="pdfFile" accept=".pdf" style="display: none; visibility: visible;">
             <input type="button" value="Browse File" class="browse-file-button" onclick="uploadPDF()">
-    
             <script>
                 function uploadPDF() {
                     document.getElementById("pdfFile").click();
@@ -71,13 +72,12 @@
 
             <input type="submit" value="Cancel" class="cancel-button">
 
-            <a href="#" class="submit-button" id="submit-button">
-                Submit
-            </a>
-            <script>
-                document.querySelector("#submit-button").addEventListener("click", function(e) {
-                    e.preventDefault(); // Prevent the default link behavior
+            <input type="submit" value="Submit" class="submit-button">
 
+
+            <script>
+                document.querySelector(".submit-button").addEventListener("click", function(e) {
+                    e.preventDefault();
                     Swal.fire({
                         title: 'Submission Confirm',
                         html: "Are you certain you want to submit this? The information you've entered <strong>cannot be altered or reversed once</strong> it's been submitted.",
@@ -86,10 +86,15 @@
                         cancelButtonText: 'Cancel',
                     }).then((result) => {
                         if (result.isConfirmed) {
-                            window.location.href = "submission.html";
+                            document.getElementById("upload-letter").submit();
+                            Swal.fire({
+                                title: 'Submission Successful',
+                                text: 'Thank you for submitting your form!',
+                                icon: 'success',
+                                confirmButtonText: 'OK'
+                            });
                         }
                     });
-
                 });
             </script>
 
--- Event Request Tracking/php/upload.php
+++ Event Request Tracking/php/upload.php
@@ -1,40 +1,69 @@
 <?php
-include 'config.php';
 session_start();
 
-if (isset($_POST['upload'])) {
-    $eventName = $_POST['eventName'];
-    $eventDate = $_POST['eventDate'];
-    $pdfName = $_FILES['reqLetter']['name'];
-    $pdfTmpName = $_FILES['reqLetter']['tmp_name'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_ba3101";
 
-    if ($_FILES['reqLetter']['error'] === UPLOAD_ERR_OK) {
-        $fileExtension = pathinfo($pdfName, PATHINFO_EXTENSION);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        $newFileName = $eventName . '.' . $fileExtension;
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-        $pdfData = file_get_contents($pdfTmpName);
-        $userID = $_SESSION['userID'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $reqEventName = $_POST["eventName"];
+    $reqEventDate = $_POST["eventDate"];
+    $userID = $_SESSION["userID"]; // Assuming you store userID in the session when the user logs in
 
-        $insertRequest = "INSERT INTO tbl_requests(reqLetter, reqEventName, reqEventDate, userID) VALUES(?, ?, ?, ?)";
-        $stmt = mysqli_prepare($conn, $insertRequest);  
-        mysqli_stmt_bind_param($stmt, "ssss", $pdfData, $eventName, $eventDate, $userID);
-        mysqli_stmt_execute($stmt);
+    // Determine the initial officeID based on your logic
+    $initialOfficeID = determineInitialOffice($userID); // Implement this function based on your business logic
 
-        $lastReqID = mysqli_insert_id($conn);
+    // Handle file upload
+    $targetDir = "uploads/"; // Adjust the target directory as needed
 
-        $reqStatus = "Pending";
-        $statusDate = date("Y-m-d");
-        $reqDeadline = date("Y-m-d", strtotime("+14 days"));
+    $targetFile = $targetDir . basename($_FILES["pdfFile"]["name"]);
 
-        $insertHistory = "INSERT INTO tbl_reqhistory(reqStatus, statusDate, reqDeadline, userID, reqID) VALUES(?, ?, ?, ?, ?)";
-        $stmt = mysqli_prepare($conn, $insertHistory);
-        mysqli_stmt_bind_param($stmt, "sssss", $reqStatus, $statusDate, $reqDeadline, $userID, $lastReqID);
-        mysqli_stmt_execute($stmt);
+    if (move_uploaded_file($_FILES["pdfFile"]["tmp_name"], $targetFile)) {
+        // File uploaded successfully, now insert data into tbl_requests
+        $reqDeadline = date("Y-m-d", strtotime($reqEventDate . "+7 days")); // Calculate reqDeadline
 
-        header('location: org.php');
+        $sql = "INSERT INTO tbl_requests (reqEventName, reqLetter, reqEventDate, userID, reqDeadline) VALUES ('$reqEventName', '$targetFile', '$reqEventDate', '$userID', '$reqDeadline')";
+
+        if ($conn->query($sql) === TRUE) {
+            // Get the ID of the last inserted request
+            $requestID = $conn->insert_id;
+
+            // Insert data into tbl_reqhistory
+            $reqStatus = "Pending";
+            $statusDate = date("Y-m-d");
+
+            $sqlHistory = "INSERT INTO tbl_reqhistory (reqStatus, statusDate, orgID, reqID, officeID) VALUES ('$reqStatus', '$reqDeadline', '$userID', '$requestID', '$initialOfficeID')";
+
+            if ($conn->query($sqlHistory) === TRUE) {
+                echo "Request submitted successfully.";
+            } else {
+                echo "Error: " . $sqlHistory . "<br>" . $conn->error;
+            }
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     } else {
-        $error[] = 'File upload error: ' . $_FILES['reqLetter']['error'];
+        echo "Error uploading file.";
     }
 }
-?>
+
+$conn->close();
+
+// Implement your logic to determine the initial officeID
+function determineInitialOffice($userID)
+{
+    // Your logic to determine the initial officeID goes here
+    // You might query the database or use some other logic to determine the officeID
+    // For now, assuming officeID is hardcoded, but you should replace it with your logic
+    return "1";
+}
